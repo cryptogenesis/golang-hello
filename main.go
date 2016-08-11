b53809088db7ee6c58808404295c2eb4aa262fcc
EVAN ROSE <evan@cryptogenesis.com>
2016-08-11 17:55:22 -0500

    test

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const (
+msg1 = "constant -> hello world"
+)
+
 func main() {
 	msg := "Hello world in golang!"
 	fmt.Println("hello", "world", "in", "golang!")
 	fmt.Println(msg)
+	fmt.Println(msg1)
 }
